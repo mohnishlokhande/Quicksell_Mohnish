debfabd710b5fe93f7b7a3f680d4eed1629b3d20
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2021-11-28 20:54:41 +0530

    frame

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -6,7 +6,26 @@ class Main extends Component {
     render(){
         return(
             <div className="mainContainer">
-                <h1>hello </h1>
+                <div className="mainCol" >
+                    <div>
+                        <p>Saving counter value</p>
+                    </div>
+                    <div className="counterClass">
+                        <div className="counerElemin">
+                            <h className="minus">-</h>
+                        </div>
+                        <div className="counerElenum">
+                            <p>10</p>
+                        </div>
+                        <div className="counerEleplus" >
+                            <h className="plus">+</h>
+                        </div>
+                    </div>
+                    <div className="counterVal">
+                            <p>Counter values : 10</p>
+                    </div>
+                </div>
+                {/* <h1>hello </h1> */}
             </div>
         );
     }
