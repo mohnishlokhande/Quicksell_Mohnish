514ef4383f54cbf5b995d3ac0822e13272e57737
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2021-11-28 14:34:23 +0530

    mainPg

---
+++ src/Component/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Main.css';
+
+class Main extends Component {
+
+    render(){
+        return(
+            <div className="mainContainer">
+                <h1>hello </h1>
+            </div>
+        );
+    }
+
+}
+
+export default Main;
