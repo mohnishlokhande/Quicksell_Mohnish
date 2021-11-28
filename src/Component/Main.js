bb979b88d2258048fc06c22f7202289d90cf468b
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2021-11-28 21:28:49 +0530

    function

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,33 +1,42 @@
-import React, {Component} from 'react';
+import React, {Component,useState} from 'react';
 import './Main.css';
 
-class Main extends Component {
 
-    render(){
-        return(
-            <div className="mainContainer">
-                <div className="mainCol">
-                    <div className="saveC">
-                        <p>Saving counter value</p>
+const Main = () => {
+    const [counter, setCounter] = useState(0)
+    
+    const handleClick1 = () => {
+        setCounter(counter + 1)
+    }
+    
+    const handleClick2 = () => {
+        setCounter(counter - 1)
+    }
+
+    return(
+        <div className="mainContainer">
+            <div className="mainCol">
+                <div className="saveC">
+                    <p>Saving counter value</p>
+                </div>
+                <div className="counterClass"  onClick={handleClick2}>
+                    <div className="counerElemin">
+                        <h className="minus">-</h>
                     </div>
-                    <div className="counterClass">
-                        <div className="counerElemin">
-                            <h className="minus">-</h>
-                        </div>
-                        <div className="counerElenum">
-                            <p>10</p>
-                        </div>
-                        <div className="counerEleplus" >
-                            <h className="plus">+</h>
-                        </div>
+                    <div className="counerElenum">
+                        <p>10</p>
                     </div>
-                    <div className="counterVal">
-                        <p>Counter values : 10</p>
+                    <div className="counerEleplus" onClick={handleClick1}>
+                        <h className="plus">+</h>
                     </div>
                 </div>
+                <div className="counterVal">
+                    <p>Counter values  : {counter}</p>
+                </div>
             </div>
-        );
-    }
+        </div>
+    );
+    
 
 }
 
